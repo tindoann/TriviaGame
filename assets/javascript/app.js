74baa096098beec4aba450fec277d41be0899a7f
tindoann <tindoann@gamil.com>
2019-07-09 09:22:55 -0400

    included js questionnaire

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+(function() {
+  let questions = [{
+  
+    question: "Which rival king attempted to take King's Landing during the Battle of the Blackwater?"
+}]
+    options: ["Ser Beric Dondarrion", "Ser Meryn Trant", "Ser Ilyn Payne"]
+    correct: 2
+}, {
+    question: ""
+}); 
