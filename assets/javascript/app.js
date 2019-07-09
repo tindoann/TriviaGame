abc7d1745088fbff7c94cc82d4806afc5ec73736
tindoann <tindoann@gamil.com>
2019-07-09 18:12:17 -0400

    display q&a with js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,27 @@
-(function() {
-  let questions = [{
-  
-    question: "Which rival king attempted to take King's Landing during the Battle of the Blackwater?"
-}]
-    options: ["Ser Beric Dondarrion", "Ser Meryn Trant", "Ser Ilyn Payne"]
-    correct: 2
-}, {
-    question: ""
-}); 
+let counter = 30; 
+let currentQuestion = 0; 
+let score = 0; 
+let lost = 0; 
+let timer; 
+
+const loadQuestion = () => {
+  let question = quizQuestions[currentQuestion].question;
+  let options = quizQuestions[currentQuestion].options;
+
+$('#time').html('Timer: ' + counter); 
+$('#game').html(`
+<div>${question}</div>
+${loadOptions(options)}
+`); 
+}
+
+const loadOptions = (options) => {
+  let result = ''; 
+
+  for (let i = 0; i < options.length; i++) {
+    result += `<p class="option" data-answer="${options[i]}">${options[i]}</p>`; 
+  }
+  return result; 
+}
+
+loadQuestion(); 
---
+++ assets/javascript/question.js
@@ -0,0 +1,37 @@
+const quizQuestions = [{
+
+  question: "Which rival king attempted to take King's Landing during the Battle of the Blackwater?",
+  options: ["Ser Beric Dondarrion", "Ser Meryn Trant", "Ser Ilyn Payne"],
+  correct: 0
+}, {
+
+  question: ["In which city does Arya Stark train to become a Faceless Man?"], 
+  options: ["Pentos", "Braavos", "Meereen"], 
+  correct: 1
+}, {
+
+  question: ["What is the name of the royal executioner who beheaded Eddard Stark for treason?"], 
+  options: ["Ser Beric Dondarrion", "Ser Ilyn Payne", "Ser Meryn Trant"], 
+  correct: 2
+
+}, {
+ question: ["Who helped Sansa Stark escape King's Landing after King Joffrey's death?"], 
+ options: ["Dontos Hollard", "Eddison Tollett", "Vardis Egen"], 
+ correct: 0
+}, {
+ question: ["Who is Lord Commander of the Kingsguard at the beginning of Game of Thrones?"], 
+ options: ["Sandor Clegane", "Gerold Clegane", "Oberyn Martell"], 
+ correct: 0
+}, {
+ question: ["Who delivered the fatal blow to the King-in-the North, Robb Stark?"], 
+ options: ["Alliser Thorne", "Alder Frey", "Roose Bolton"], 
+ correct: 2
+}, {
+ question: ["Who was the Mad King's firstborn son?"], 
+ options: ["Aemon Targaryen", "Aegon Targaryen", "Rhaegar Targaryen"], 
+ correct: 2
+}, {
+ question: ["How many times has Sansa Stark been married?"], 
+ options: ["1", "2", "3"], 
+ correct: 1
+}]; 
